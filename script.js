fa357783d6029e25a685c34b5b0bc69a6d7a1586
Lorena <lorenadomingos@proton.me>
2026-03-24 00:51:18 -0300

    Add password validation UI in index.html and implement password strength checking logic in script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function passwordValidation(password) {
+    const passwordInput = document.getElementById('passwordInput');
+    const outputContent = document.querySelector('.outputContent');
+
+    password = passwordInput.value;
+
     let strongPoints = 0;
 
     if (password.length >= 8) {
@@ -26,6 +31,31 @@ function passwordValidation(password) {
             strongPoints -= 1;
             break;
         };
+    }
+
+    if (password.length < 8) {
+        strongPoints = 0;
+        outputContent.innerHTML = "Your password needs to be at least 8 characters."
+    }
+
+    if (password.length === 0) {
+        outputContent.innerHTML = "Please, write something!";
+    } else {
+        if (strongPoints === 5) {
+            outputContent.innerHTML = "Strong";
+        }
+
+        if (strongPoints >= 1 && strongPoints <= 2) {
+            outputContent.innerHTML = "Too Weak";
+        }
+
+        if (strongPoints === 3) {
+            outputContent.innerHTML = "Medium";
+        }
+
+        if (strongPoints === 4) {
+            outputContent.innerHTML = "Nice";
+        }
     }
 
     return strongPoints;
